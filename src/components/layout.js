245fb76d4efcfbdf3e35832fae5a61acff9c0205
Megan <13woodruffm@gmail.com>
2021-05-19 09:34:50 -0500

    Initial repo setup

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,38 +10,30 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
+import "./layout.scss"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
+  // const data = useStaticQuery(graphql`
+  //   query SiteTitleQuery {
+  //     site {
+  //       siteMetadata {
+  //         title
+  //       }
+  //     }
+  //   }
+  // `)
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+      {/* <Header siteTitle={data.site.siteMetadata?.title || `Title`} /> */}
+      <div>
         <main>{children}</main>
         <footer
-          style={{
-            marginTop: `2rem`,
-          }}
+        // style={{
+        //   marginTop: `2rem`,
+        // }}
         >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
+          © {new Date().getFullYear()}
         </footer>
       </div>
     </>
--- src/components/seo.js
+++ src/components/seo.js
@@ -69,7 +69,13 @@ function Seo({ description, lang, meta, title }) {
           content: metaDescription,
         },
       ].concat(meta)}
-    />
+    >
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap"
+        rel="stylesheet"
+      />
+    </Helmet>
   )
 }
 
